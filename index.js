0db757637765c87f871ada95c2819100a8434f42
rajaramalingam <rajaramalingam345@gmail.com>
2017-10-02 10:19:07 +0530

    2 cmt

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 	 document.getElementById("numbers").innerHTML += i + "<br>"
 	}
 
-	 function arry(){
+	
+function arry(){
  	var place=["chennai","pondy","tricy","covi","madurai"];
  	var len=place.length;
  	var text="";
@@ -14,3 +15,19 @@
  	}
 document.getElementById("result").innerHTML=text;
  }
+
+
+ function findValue() {
+	var arr=["1","2","3","12","3","12","14","15","6","7","8","3","4","1"];
+	var a=document.getElementById('x').value;
+	var b=0;
+	var i;
+	for(i=0; i<arr.length; i++){
+		if(arr[i]==a){
+			b++;
+		}
+	}
+	document.getElementById("x").innerHTML=a;
+	document.getElementById("res").innerHTML=b;
+
+}
